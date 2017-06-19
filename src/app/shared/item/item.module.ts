4ad8c74f7e4179894cd59076feaa0690b00447f4
Jeffarese <jeff@wallapop.com>
2017-06-19 17:25:27 +0200

    Resolve dependency problems

--- src/app/shared/item/item.module.ts
+++ src/app/shared/item/item.module.ts
@@ -2,23 +2,13 @@ import { NgModule } from '@angular/core';
 import { ItemService } from './item.service';
 import { MdIconModule } from '@angular/material';
 import { TrackingModule } from '../tracking/tracking.module';
-import { ItemAvatarComponent } from './item-avatar/item-avatar.component';
 import { SharedModule } from '../../shared/shared.module';
-import { ItemSoldComponent } from './item-sold/item-sold.component';
 
 @NgModule({
   imports: [
     SharedModule,
     MdIconModule,
     TrackingModule
-  ],
-  exports: [
-    ItemAvatarComponent,
-    ItemSoldComponent
-  ],
-  declarations: [
-    ItemAvatarComponent,
-    ItemSoldComponent
   ],
   providers: [
     ItemService
